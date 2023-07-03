b2c91ecda0e397394eacc08f2f4d3e318bfcf47a
programmer-b <dancan219w@gmail.com>
2023-07-03 16:45:17 +0300

    init push

---
+++ pages/products/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="grid grid-cols-4 gap-5">
+            <div v-for="p in products">
+<!--                <NuxtLink :to = "`/products/${p.id}`" > {{p.title}} </NuxtLink>-->
+                <ProductCard :product = "p"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: 'products'
+})
+//Fetch products from API
+const {data: products} = await useFetch('https://fakestoreapi.com/products');
+</script>
+
+<style scoped>
+
+</style>
